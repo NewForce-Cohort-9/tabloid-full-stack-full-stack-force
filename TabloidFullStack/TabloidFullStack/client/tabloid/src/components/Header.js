7c8c53765e7402aa393f7e1a49bd62affae29611
scof00 <scofprofessional@gmail.com>
2024-09-17 11:42:21 -0400

    Finished Ticket #9

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Header.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Header.js
@@ -29,6 +29,14 @@ export default function Header({isLoggedIn, setIsLoggedIn}) {
               </NavItem>
             }
           </Nav>
+          <Nav className="mr-auto" navbar>
+            { /* When isLoggedIn === true, we will render the Home link */ }
+            {isLoggedIn &&
+              <NavItem>
+                <NavLink tag={RRNavLink} to="/categories">Category Management</NavLink>
+              </NavItem>
+            }
+          </Nav>
           <Nav navbar>
             {isLoggedIn &&
               <>
