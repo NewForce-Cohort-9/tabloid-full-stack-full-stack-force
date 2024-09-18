7a575bd4321e4909ce31feee1694a3bc91e8f3ae
Fiona Wee <fwee@oswego.edu>
2024-09-18 08:37:42 -0400

    delete ticket

--- TabloidFullStack/TabloidFullStack/Controllers/CommentController.cs
+++ TabloidFullStack/TabloidFullStack/Controllers/CommentController.cs
@@ -1,170 +1,81 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
-using System.Security.Claims;
+﻿using Microsoft.AspNetCore.Mvc;
 using TabloidFullStack.Models;
 using TabloidFullStack.Repositories;
 
-
 namespace TabloidFullStack.Controllers
-{
-    public class CommentController : Controller
     {
-        private readonly ICommentRepository _commentRepository;
-        private readonly IPostRepository _postRepository;
-        private readonly IUserRepository _userRepository;
-
-
-        public CommentController(ICommentRepository commentRepository, IPostRepository postRepository, IUserRepository userRepository)
+        [Route("api/[controller]")]
+        [ApiController]
+        public class CommentController : ControllerBase
         {
-            _commentRepository = commentRepository;
-            _postRepository = postRepository;
-            _userRepository = userRepository;
-        }
+            private readonly ICommentRepository _commentRepository;
+            private readonly IPostRepository _postRepository;
+            private readonly IUserRepository _userRepository;
 
-        // GET: CommentController
-        public ActionResult Index(int postId)
-        {
-
-            var comments = _commentRepository.GetCommentsByPostId(postId);
-            var post = _postRepository.GetPublishedPostById(postId); // Fetch post details
-
-            // Get the current user's ID from authentication 
-            var currentUserIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-            // Convert the current user's ID to an integer
-            int currentUserId = int.Parse(currentUserIdString);
-
-            var viewModel = new CommentViewModel
+            public CommentController(ICommentRepository commentRepository, IPostRepository postRepository, IUserRepository userRepository)
             {
-                PostId = postId,
-                PostTitle = post.Title,
-                Comments = comments,
-                CurrentUserId = currentUserId // Pass the current user’s ID to the view model
-            };
-
-            return View(viewModel);
-        }
-
-        // GET: CommentController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Comment/Create
-        public ActionResult Create(int postId)
-        {
-            var comment = new Comment
-            {
-                PostId = postId,
-                UserProfileId = GetCurrentUserProfileId()
-            };
-
-            return View(comment);
-        }
-
-        // POST: Comment/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Comment comment)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    comment.CreateDateTime = DateTime.Now;
-                    comment.UserProfileId = GetCurrentUserProfileId();
-
-                    _commentRepository.Add(comment);
-
-                    return RedirectToAction("Index", new { postId = comment.PostId });
-                }
-                catch (Exception ex)
-                {
-
-                    ModelState.AddModelError("", "An error occurred while creating the comment. Please try again.");
-                    Console.WriteLine("Error creating comment: " + ex.Message);
-
-                }
+                _commentRepository = commentRepository;
+                _postRepository = postRepository;
+                _userRepository = userRepository;
             }
 
-            return View(comment);
-
-        }
-
-
-        // GET: Comment/Edit/5
-        public IActionResult Edit(int id)
-        {
-            var comment = _commentRepository.GetCommentById(id);
-
-            if (comment == null)
+            // GET: api/Comment?postId=1
+            [HttpGet]
+            public IActionResult GetAll(int postId)
             {
-                return NotFound();
+                return Ok(_commentRepository.GetCommentsByPostId(postId));
             }
 
-            return View(comment);
-
-        }
-
-        // POST: Comment/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Comment comment)
-        {
-
-            if (ModelState.IsValid)
+            // GET: api/Comment/{id}
+            [HttpGet("{id}")]
+            public IActionResult Get(int id)
             {
-                try
+                var comment = _commentRepository.GetCommentById(id);
+                if (comment == null)
                 {
-                    _commentRepository.Edit(id, comment);
-
-                    return RedirectToAction("Index", new { postId = comment.PostId });
-                    // Note: going back to Index, so look up at Index method in controller, it takes postId parameter so MUST HAVE ,new { postId = postId }
+                    return NotFound();
                 }
-                catch (Exception ex)
-                {
-                    ModelState.AddModelError("", "An error occurred while updating the comment. Please try again.");
-
-                }
-            }
-
-            return View(comment);
+                return Ok(comment);
         }
 
-        // GET: CommentController/Delete/5
+        //// POST: api/Comment/Post
+        /// Swagger test:
+        ///{
+        //  "subject": "Test Subject",
+        //  "content": "Test content",
+        //  "postId": 1
+        //}
+    [HttpPost]
+        public IActionResult Post(Comment comment)
+        {
+           
+            comment.CreateDateTime = DateTime.Now;
+            //comment.UserProfileId =client side get user profile from local storage 
+            _commentRepository.Add(comment);
+            return CreatedAtAction("GetAll", new { id = comment.Id }, comment);
+        }
+
+
+        // DELETE api/<CommentController>/5
+        [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var comment = _commentRepository.GetCommentById(id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
-            return View(comment);
-        }
-
-        // POST: CommentController/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var comment = _commentRepository.GetCommentById(id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
             _commentRepository.Delete(id);
-            return RedirectToAction("Index", new { postId = comment.PostId });
+            return NoContent();
         }
 
-
-
-
-        private int GetCurrentUserProfileId()
-        {
-            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            return int.Parse(id);
-        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+    
--- TabloidFullStack/TabloidFullStack/Models/Comment.cs
+++ TabloidFullStack/TabloidFullStack/Models/Comment.cs
@@ -15,12 +15,12 @@ namespace TabloidFullStack.Models
 
         [DisplayName("Author")]
         public int UserProfileId { get; set; }
-        public UserProfile? UserProfile { get; set; } // Use UserProfile.DisplayName in Comment's Index.cshtml
+        public UserProfile? UserProfile { get; set; } 
 
         [DisplayName("Created on")]
         public DateTime CreateDateTime { get; set; }
 
-        public Post? Post { get; set; } // Use Post.Title in Comment's Index.cshtml
+        public Post? Post { get; set; } 
         public int PostId { get; set; }
 
 
