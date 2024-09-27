8b4ed277605263c08709c69f1ef36bbc5b8507e8
scof00 <scofprofessional@gmail.com>
2024-09-27 14:21:35 -0400

    Finished ticket 39

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/UserView.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/UserView.js
@@ -8,21 +8,43 @@ import PostDetail from "./Post/PostDetail";
 import UserProfileList from "./UserProfiles/UserProfileList";
 import Header from "./Header";
 import UserHeader from "./UserHeader";
+import PostForm from "./Post/PostForm";
+import PostDetails from "./Post/PostDetail";
+import EditPostForm from "./Post/EditPostForm";
+import { CommentForm } from "./Comments/CommentForm";
+import { DeleteComment } from "./Comments/DeleteComment";
 
 export const UserView = (isLoggedIn, setIsLoggedIn) => {
   return (
     <Routes>
-        <Route
+      <Route
         path="/"
         element={
-            <>
+          <>
             <UserHeader isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
             <Outlet />
-            </>
-        }
-        >
-        
-        </Route>
+           
+          </>
+        }>
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/myposts" element={<MyPosts />} />
+        <Route path="/posts/create" element={<PostForm />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        <Route path="/posts/edit/:postId" element={<EditPostForm />} />
+        <Route path="/posts/:postId/comments" element={<CommentList />} />
+        <Route
+          path="/posts/:postId/comments/add"
+          element={<CommentForm />}
+        />
+        <Route
+          path="/posts/:postId/comments/delete/:commentId"
+          element={<DeleteComment />}
+        />
+        <Route
+          path="/posts/:postId/comments/edit/:commentId"
+          element={<CommentForm />}
+        />
+      </Route>
     </Routes>
   );
 };
